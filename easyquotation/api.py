32bb63759b7b4f445ef77a59c522d9318f0042d6
marx99 <maxingzhe99@126.com>
2017-05-09 12:38:26 +0800

    delete other(easyquotation api)

--- easyquotation/api.py
+++ easyquotation/api.py
@@ -1,10 +1,10 @@
 # coding:utf8
 import sys
 
-from .boc import Boc
-from .jsl import Jsl
-from .leverfun import Leverfun
-from .sina import Sina
+#from .boc import Boc
+#from .jsl import Jsl
+#from .leverfun import Leverfun
+#from .sina import Sina
 from .tencent import Tencent
 
 PY_VERSION = sys.version_info[:2]
@@ -13,13 +13,13 @@ if PY_VERSION < (3, 5):
 
 
 def use(source):
-    if source in ['sina']:
-        return Sina()
-    if source in ['leverfun', 'lf']:
-        return Leverfun()
-    if source in ['jsl']:
-        return Jsl()
+#    if source in ['sina']:
+#        return Sina()
+#    if source in ['leverfun', 'lf']:
+#        return Leverfun()
+#    if source in ['jsl']:
+#        return Jsl()
     if source in ['qq', 'tencent']:
         return Tencent()
-    if source in ['boc']:
-        return Boc()
+#    if source in ['boc']:
+#        return Boc()
